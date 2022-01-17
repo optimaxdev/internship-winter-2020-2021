db1aca71579e474a61416c6431a59fd65e7409a4
NatawaSergeevna <nett0daynet@gmail.com>
2022-01-17 16:54:47 +0400

    changed natasha.css, natasha.jsx, App.js

--- src/components/natasha.jsx
+++ src/components/natasha.jsx
@@ -1,6 +1,7 @@
-export const Anton = () => {
-   return <div clssNsme="container">
-       <p className="nameNatasha">Наташа</p>
-       <img className="imgNatasha" src={"https://i.ytimg.com/vi/rIwGkC8QjyM/hqdefault.jpg"} />
+import './natasha.css';
+export const Natasha = () => {
+   return <div className="container">
+       <p className="nameNatasha"><b>Наташа</b></p>
+       <img className="imgNatasha" src={"https://avtozvuk-info.ru/wp-content/uploads/2018/07/post_5b5a9581a79a8.jpeg"} />
    </div>
 }
