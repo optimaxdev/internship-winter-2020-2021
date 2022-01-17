c16ded8ac4a5ed26136acb5e1608022bd0edab96
NatawaSergeevna <nett0daynet@gmail.com>
2022-01-17 15:44:39 +0400

    added natasha.css & natasha.jsx in component, changed App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import {Anton} from "./components/anton";
+import {Natasha} from "./components/natasha";
 
 export const App = () => {
   return <div>
     <Anton />
+    <Natasha />
   </div>
 }
---
+++ src/components/natasha.jsx
@@ -0,0 +1,6 @@
+export const Anton = () => {
+   return <div clssNsme="container">
+       <p className="nameNatasha">Наташа</p>
+       <img className="imgNatasha" src={"https://i.ytimg.com/vi/rIwGkC8QjyM/hqdefault.jpg"} />
+   </div>
+}
